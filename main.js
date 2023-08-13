74dbb0e1b29ba8ec628df18bec9fc78e0b6c4a72
angelina0425 <107446432+angelina0425@users.noreply.github.com>
2023-08-12 20:42:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Bedroom(){
+    window.location = "bedroom.html"
+}
+
+function Tv_Ac(){
+    window.location = "tv_ac.html"
+}
+
+function Desk(){
+    window.location = "desk.html"
+}
+
+function Bottles(){
+    window.location = "bottles.html"
+}
+
+function Fruit_Basket(){
+    window.location = "fruit_basket.html"
+}
